d040e20d780090d399a902e114a1c60836cf1fb2
CELSO MAGALHAES FERREIRA <irmaocelsinho@gmail.com>
2019-05-07 16:30:21 -0300

    inicial

--- www/js/index.js
+++ www/js/index.js
@@ -1,51 +1,43 @@
+/*var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+
+
+		
 $(function(){
 	
-	//window.location.href = 'http://cadigbh.atspace.cc/PreInscricaoJovensMicroregional/';
-	//var ref = cordova.InAppBrowser.open('http://cadigbh.atspace.cc/PreInscricaoJovensMicroregional/', '_self', 'location=yes');
-	
-	//document.addEventListener("deviceready", onDeviceReady, false);
-	
-	
-	//bindClicks();
 
 	
 });
-
-/*function bindClicks(){
-	console.log('bindClicks(');
 	
-	$('a').each(function(){
-		
-		console.log('unbind click');
-		$(this).unbind( "click" );
-		
-		console.log('bind click');
-		$(this).click(function(ev){
-			ev.stopPropagation();
-			ev.stopPropagation();
-			ev.preventDefault();
-			href = $(this).attr('href');
-			
-			console.log('$.mobile.loadPage');
-			$.mobile.loadPage( href );
-			
-			
-			/*$.ajax({
-					url: href,
-					success: function(data){
-								pageId = $.mobile.activePage.attr("id");
-								$("#"+pageId+' div[data-role="content"]').html(data);
-								bindClicks();
-							},
-					dataType: 'jsonp'
-				});*/
-			
-			
-			/*$.get(href, function(data){
-				pageId = $.mobile.activePage.attr("id");
-				$("#"+pageId+' div[data-role="content"]').html(data);
-				bindClicks();
-			});*/
-		/*})
-	});
-}*/
+$(document).on('pagechange',function(){
+
+	
+
+});*/
+
+app.initialize();

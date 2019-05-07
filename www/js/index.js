0276b46c3cc57661de0c935cb099570f31ede7bb
CELSO MAGALHAES FERREIRA <irmaocelsinho@gmail.com>
2019-05-07 14:24:43 -0300

    alteracao

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,13 @@ $(function(){
 	//window.location.href = 'http://cadigbh.atspace.cc/PreInscricaoJovensMicroregional/';
 	//var ref = cordova.InAppBrowser.open('http://cadigbh.atspace.cc/PreInscricaoJovensMicroregional/', '_self', 'location=yes');
 	
+	document.addEventListener("deviceready", onDeviceReady, false);
+	
 });
+
+function onDeviceReady() {
+	window.open = cordova.InAppBrowser.open;
+}
 	
 /*$(document).on('pagechange',function(){
 

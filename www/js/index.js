3c26a20e1317bd47cf1742883a8392c0e90da548
CELSO MAGALHAES FERREIRA <irmaocelsinho@gmail.com>
2019-05-07 16:09:26 -0300

    com iframes

--- www/js/index.js
+++ www/js/index.js
@@ -3,16 +3,49 @@ $(function(){
 	//window.location.href = 'http://cadigbh.atspace.cc/PreInscricaoJovensMicroregional/';
 	//var ref = cordova.InAppBrowser.open('http://cadigbh.atspace.cc/PreInscricaoJovensMicroregional/', '_self', 'location=yes');
 	
-	document.addEventListener("deviceready", onDeviceReady, false);
+	//document.addEventListener("deviceready", onDeviceReady, false);
+	
+	
+	//bindClicks();
+
 	
 });
 
-function onDeviceReady() {
-	window.open = cordova.InAppBrowser.open;
-}
+/*function bindClicks(){
+	console.log('bindClicks(');
 	
-/*$(document).on('pagechange',function(){
-
-	
-
-});*/
+	$('a').each(function(){
+		
+		console.log('unbind click');
+		$(this).unbind( "click" );
+		
+		console.log('bind click');
+		$(this).click(function(ev){
+			ev.stopPropagation();
+			ev.stopPropagation();
+			ev.preventDefault();
+			href = $(this).attr('href');
+			
+			console.log('$.mobile.loadPage');
+			$.mobile.loadPage( href );
+			
+			
+			/*$.ajax({
+					url: href,
+					success: function(data){
+								pageId = $.mobile.activePage.attr("id");
+								$("#"+pageId+' div[data-role="content"]').html(data);
+								bindClicks();
+							},
+					dataType: 'jsonp'
+				});*/
+			
+			
+			/*$.get(href, function(data){
+				pageId = $.mobile.activePage.attr("id");
+				$("#"+pageId+' div[data-role="content"]').html(data);
+				bindClicks();
+			});*/
+		/*})
+	});
+}*/
